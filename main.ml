fa37b7101d3eb5cd406414c88c296134827205fd
William M. Leiserson <willtor@mit.edu>
2016-08-05 14:03:17 -0400

    Do a tiny amount of input handling.

--- main.ml
+++ main.ml
@@ -4,11 +4,18 @@ open Deflex
 open Unparse
 
 let main () =
-  try
-    let stmts = ((defparse deflex) (Lexing.from_channel stdin))
-    in unparse stmts
-  with LexError err ->
-    print_endline err; exit 1
+  if (Array.length Sys.argv - 1) = 0 then
+    (print_endline "Need an input file.";
+     exit 1)
+  else
+    let infile = try open_in Sys.argv.(1)
+      with _ -> print_endline "Unable to open input file."; exit 1
+    in
+    try
+      let stmts = ((defparse deflex) (Lexing.from_channel infile))
+      in unparse stmts
+    with LexError err ->
+      print_endline err; exit 1
 
 let _ = main ()
 
