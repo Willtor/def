a76b6abd88441027040b070174a6d98b61859b7a
William M. Leiserson <willtor@mit.edu>
2016-08-05 14:27:37 -0400

    Get the filename into error messages.

--- main.ml
+++ main.ml
@@ -1,7 +1,19 @@
 open Ast
 open Defparse
 open Deflex
+open Lexing
 open Unparse
+
+let set_fname file lexbuf =
+  lexbuf.lex_start_p <- { pos_fname = file;
+                          pos_lnum = lexbuf.lex_start_p.pos_lnum;
+                          pos_bol = lexbuf.lex_start_p.pos_bol;
+                          pos_cnum = lexbuf.lex_start_p.pos_cnum };
+  lexbuf.lex_curr_p <- { pos_fname = file;
+                         pos_lnum = lexbuf.lex_curr_p.pos_lnum;
+                         pos_bol = lexbuf.lex_curr_p.pos_bol;
+                         pos_cnum = lexbuf.lex_curr_p.pos_cnum };
+  lexbuf
 
 let main () =
   if (Array.length Sys.argv - 1) = 0 then
@@ -12,8 +24,9 @@ let main () =
       with _ -> print_endline "Unable to open input file."; exit 1
     in
     try
-      let stmts = ((defparse deflex) (Lexing.from_channel infile))
-      in unparse stmts
+      let lexbuf = set_fname Sys.argv.(1) (Lexing.from_channel infile) in
+      let stmts = ((defparse deflex) lexbuf)
+      in unparse stmts; close_in infile
     with LexError err ->
       print_endline err; exit 1
 
