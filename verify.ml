b3ef1ee0c6ce72c1e2e6fe828a29b4a28ea11c76
William M. Leiserson <willtor@mit.edu>
2016-08-09 16:29:24 -0400

    Checkpoint: towards verification.

--- verify.ml
+++ verify.ml
@@ -1,14 +1,25 @@
 open Ast
 
-let verify_stmt = function
-  | StmtExpr e -> StmtExpr e
-  | Block stmt_list -> Block stmt_list
-  | DefFcn (fn, stmt) -> DefFcn (fn, stmt)
-  | Return e -> Return e
+let verify_expr e = e
+
 
 let verify_ast =
   let rec verify accum = function
     | stmt :: rest ->
+       let rec verify_stmt = function
+         | StmtExpr e ->
+            let verified_e = verify_expr e in StmtExpr verified_e
+         | Block stmt_list -> 
+            let verified_stmt_list = verify [] stmt_list
+            in Block verified_stmt_list
+         | DefFcn (fn, stmt) ->
+            let verified_fn = fn in
+            let verified_stmt = verify_stmt stmt in
+            DefFcn (verified_fn, verified_stmt)
+         | Return e ->
+            let verified_e = verify_expr e in Return verified_e
+       in
        verify (verify_stmt stmt :: accum) rest
     | [] -> List.rev accum
   in verify []
+
