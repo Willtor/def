d6058e92dd37094a01be9966dffe677ae32b8905
William M. Leiserson <willtor@mit.edu>
2016-08-11 16:32:22 -0400

    Documentation.

--- util.ml
+++ util.ml
@@ -1,12 +1,16 @@
 
 open Lexing
 
+(* Take a Lexing.position and generate a string of the form:
+   "path/to/file.def (line n column n)" *)
 let format_position pos =
   pos.pos_fname
   ^ " (line " ^ (string_of_int pos.pos_lnum)
   ^ " column " ^ (string_of_int (pos.pos_cnum - pos.pos_bol))
   ^ ")"
 
+(* Take a Lexing.position and return a 2-line string: the source line, and a
+   line with a carat underneath the offending column. *)
 let show_source pos =
   let infile = open_in pos.pos_fname in
   let rec find_line n =
--- util.mli
+++ util.mli
@@ -1,2 +1,8 @@
+
+(* Take a Lexing.position and generate a string of the form:
+   "path/to/file.def (line n column n)" *)
 val format_position : Lexing.position -> string
+
+(* Take a Lexing.position and return a 2-line string: the source line, and a
+   line with a carat underneath the offending column. *)
 val show_source : Lexing.position -> string
