abef5dc432de7eacee6e352571172f9205c8877f
William M. Leiserson <willtor@mit.edu>
2016-08-12 01:13:28 -0400

    Add symbol table interface to Util.

--- util.ml
+++ util.ml
@@ -1,16 +1,18 @@
 
 open Lexing
 
-(* Take a Lexing.position and generate a string of the form:
-   "path/to/file.def (line n column n)" *)
+type 'a symtab = (string, 'a) Hashtbl.t list
+
+(** Take a Lexing.position and generate a string of the form:
+    "path/to/file.def (line n column n)" *)
 let format_position pos =
   pos.pos_fname
   ^ " (line " ^ (string_of_int pos.pos_lnum)
   ^ " column " ^ (string_of_int (pos.pos_cnum - pos.pos_bol))
   ^ ")"
 
-(* Take a Lexing.position and return a 2-line string: the source line, and a
-   line with a carat underneath the offending column. *)
+(** Take a Lexing.position and return a 2-line string: the source line, and
+    a line with a carat underneath the offending column. *)
 let show_source pos =
   let infile = open_in pos.pos_fname in
   let rec find_line n =
@@ -23,3 +25,43 @@ let show_source pos =
     (find_line (pos.pos_lnum - 1)) ^ "\n"
     ^ (String.make (pos.pos_cnum - pos.pos_bol) ' ') ^ "^"
   with _ -> "(Err: Unable to read from " ^ pos.pos_fname ^ ")"
+
+(****************************************************************************)
+(*                          Symbol Table Functions                          *)
+(****************************************************************************)
+
+let make_symtab_layer () = Hashtbl.create 32
+
+(** Create a new symbol table with a global scope. *)
+let make_symtab () =
+  [make_symtab_layer ()]
+
+(** Push a scope level onto a symbol table. *)
+let push_symtab_scope symtab = (make_symtab_layer ()) :: symtab
+
+(** Add a symbol to the symbol table at the deepest scope. *)
+let add_symbol symtab name value =
+  match symtab with
+  | scope :: _ -> Hashtbl.add scope name value
+  | [] -> failwith "Internal error: No symbol table scopes."
+
+(** Lookup a symbol at the deepest scope-only.  This is useful for checking
+    for redefinitions of a variable when shadowing is okay. *)
+let lookup_symbol_local symtab name =
+  match symtab with
+  | scope :: _ ->
+     begin
+       try Some (Hashtbl.find scope name) with Not_found -> None
+     end
+  | [] -> failwith "Internal error: No symbol table scopes."
+
+(** Lookup a symbol, starting from the deepest scope and working outward. *)
+let rec lookup_symbol symtab name =
+  match symtab with
+  | [] -> failwith "Internal error: No symbol table scopes."
+  | [global] -> lookup_symbol_local symtab name
+  | scope :: rest ->
+     begin match lookup_symbol_local symtab name with
+     | Some _ as ret -> ret
+     | None -> lookup_symbol rest name
+     end
--- util.mli
+++ util.mli
@@ -1,8 +1,30 @@
 
-(* Take a Lexing.position and generate a string of the form:
-   "path/to/file.def (line n column n)" *)
+type 'a symtab
+
+(** Take a Lexing.position and generate a string of the form:
+    "path/to/file.def (line n column n)" *)
 val format_position : Lexing.position -> string
 
-(* Take a Lexing.position and return a 2-line string: the source line, and a
-   line with a carat underneath the offending column. *)
+(** Take a Lexing.position and return a 2-line string: the source line, and
+    a line with a carat underneath the offending column. *)
 val show_source : Lexing.position -> string
+
+(****************************************************************************)
+(*                          Symbol Table Functions                          *)
+(****************************************************************************)
+
+(** Create a new symbol table with a global scope. *)
+val make_symtab : unit -> 'a symtab
+
+(** Push a scope level onto a symbol table. *)
+val push_symtab_scope : 'a symtab -> 'a symtab
+
+(** Add a symbol to the symbol table at the deepest scope. *)
+val add_symbol : 'a symtab -> string -> 'a -> unit
+
+(** Lookup a symbol at the deepest scope-only.  This is useful for checking
+    for redefinitions of a variable when shadowing is okay. *)
+val lookup_symbol_local : 'a symtab -> string -> 'a option
+
+(** Lookup a symbol, starting from the deepest scope and working outward. *)
+val lookup_symbol : 'a symtab -> string -> 'a option
