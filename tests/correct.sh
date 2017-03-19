598dbc80a69ffd0958f5134031f623cfd9c62143
William M. Leiserson <willtor@mit.edu>
2017-03-19 11:37:24 -0400

    Documentation and expansion of correct.sh script.

--- tests/correct.sh
+++ tests/correct.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+## Test a simple program's output against a correct reference output.  The
+## program should succeed in running and not crash.
+
+## Usage: correct.sh <program>
+## The files, <program> and <program>.correct, should exist.
+
 prog=./$1
 correct=$1.correct
 output=$1.out
@@ -9,6 +15,12 @@ echo "### Correctness Test: $1 ###"
 
 sleep 0.2
 eval $prog > $output
+code=$?
+
+if [ "$code" != "0" ]; then
+    echo "Fail: program terminated with code $code."
+    exit 1
+fi
 
 result=`diff $correct $output`
 
@@ -17,7 +29,7 @@ if [ "$result" = "" ]; then
     echo
     exit 0
 else
-    echo "Fail:"
+    echo "Fail: Output differed from reference."
     diff $correct $output
     exit 1
 fi
