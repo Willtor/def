e450c8369891a18696b2efd38d15e830ce0d3443
William M. Leiserson <willtor@mit.edu>
2017-04-04 12:15:35 -0400

    Ability to emit array types in C header files.

--- src/header.ml
+++ src/header.ml
@@ -37,15 +37,27 @@ let ctype_of =
   fun name ->
     try Hashtbl.find map name with _ -> name
 
-let rec cstring_of_type = function
-  | VarType (_, nm) -> ctype_of nm
+let rec unparse_expr = function
+  | ExprLit (_, LitBool b) -> string_of_bool b
+  | ExprLit (_, LitI8 c) | ExprLit (_, LitU8 c) ->
+     "'" ^ (Char.escaped c) ^ "'"
+  | ExprLit (_, LitI16 n) | ExprLit (_, LitU16 n)
+    | ExprLit (_, LitI32 n) | ExprLit (_, LitU32 n) ->
+     Int32.to_string n
+  | ExprLit (_, LitI64 n) | ExprLit (_, LitU64 n) ->
+     Int64.to_string n
+  | _ -> Report.err_internal __FILE__ __LINE__
+                             "Don't know how to unparse complex exprs."
+
+let rec cstring_of_type accum = function
+  | VarType (_, nm) -> (ctype_of nm) ^ accum
   | FcnType _ -> Report.err_internal __FILE__ __LINE__
      "FcnType not implemented."
   | StructType _ -> Report.err_internal __FILE__ __LINE__
      "StructType not implemented."
-  | ArrayType _ -> Report.err_internal __FILE__ __LINE__
-     "ArrayType not implemented."
-  | PtrType (_, t) -> (cstring_of_type t) ^ "*"
+  | ArrayType (_, e, t) ->
+     (cstring_of_type (accum ^ "[" ^ (unparse_expr e) ^ "]") t)
+  | PtrType (_, t) -> (cstring_of_type ("*" ^ accum) t)
   | Ellipsis _ -> "..."
 
 let output_typedefs oc = function
@@ -62,8 +74,8 @@ let output_structs oc = function
        output_string oc ("struct " ^ nm ^ " {\n");
        List.iter
          (fun (_, mname, tp) ->
-           output_string oc ("  " ^ (cstring_of_type tp) ^ " "
-                             ^ mname ^ ";\n"))
+           output_string oc ("  " ^ (cstring_of_type (" " ^ mname) tp)
+                             ^ ";\n"))
          mlist;
        output_string oc "};\n\n"
      end
@@ -80,12 +92,12 @@ let output_functions oc = function
           output_string oc (comment ^ "\n")
      in
      begin
-       output_string oc (cstring_of_type ret);
-       output_string oc (" " ^ name ^ "(");
+       output_string oc (cstring_of_type (" " ^ name) ret);
+       output_string oc "(";
        let params =
          List.map (fun (_, name, tp) ->
-           (cstring_of_type tp) ^ (if name <> "" then " " ^ name else ""))
-           args
+             cstring_of_type (if name <> "" then " " ^ name else "") tp)
+                  args
        in
        output_string oc (String.concat ", " params);
        output_string oc ");\n\n"
