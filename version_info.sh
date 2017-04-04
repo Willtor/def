cfca410effa4a8314f3b3c7c0aae0cf9f31dc048
William M. Leiserson <willtor@mit.edu>
2017-04-04 12:16:01 -0400

    Bump the minor version.

--- version_info.sh
+++ version_info.sh
@@ -4,8 +4,8 @@
 # across the compiler and libraries.
 
 VERSION_MAJOR=0
-VERSION_MINOR=1
-VERSION_PATCH=1
+VERSION_MINOR=2
+VERSION_PATCH=0
 VERSION_SUFFIX=
 VERSION_BUILD=`git rev-parse HEAD | cut -c -12`
 BUILD_DATE=`date -I`
