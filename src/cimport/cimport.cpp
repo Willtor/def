9f506cb095ad9dd5958830ee9c1117d0b7433b84
William M. Leiserson <willtor@mit.edu>
2018-07-09 12:07:18 -0400

    cimport: interfacing with OCaml... play nice with the garbage collector.

--- src/cimport/cimport.cpp
+++ src/cimport/cimport.cpp
@@ -50,14 +50,17 @@ static map<SourceLocation, string> fund_types;
 
 static value reverse_list (value list)
 {
-    value reversed_list = Val_int(0);
+    CAMLparam1 (list);
+    CAMLlocal1 (reversed_list);
+    reversed_list = Val_int(0);
     while (list != Val_int(0)) {
-        value cdr = Field(list, 1);
+        CAMLlocal1 (cdr);
+        cdr = Field(list, 1);
         Store_field(list, 1, reversed_list);
         reversed_list = list;
         list = cdr;
     }
-    return reversed_list;
+    CAMLreturn(reversed_list);
 }
 
 class DeclVisitor : public clang::RecursiveASTVisitor<DeclVisitor> {
@@ -111,35 +114,46 @@ private:
      */
     value position_of_SourceLocation (const SourceLocation src_loc)
     {
+        CAMLparam0 ();
+
         SourceLocation spelling_loc = sm.getSpellingLoc(src_loc);
         unsigned int file_offset = sm.getFileOffset(spelling_loc);
         unsigned int column = sm.getSpellingColumnNumber(src_loc) - 1;
 
-        value pos = caml_alloc(4, 0);
-        Store_field(pos, 0,
-                    caml_copy_string(sm.getFilename(spelling_loc).str()
-                                     .c_str()));
+        CAMLlocal2 (pos, ocamlstr);
+        pos = caml_alloc(4, 0);
+        ocamlstr = caml_copy_string(sm.getFilename(spelling_loc).str()
+                                    .c_str());
+
+        Store_field(pos, 0, ocamlstr);
         Store_field(pos, 1, Val_int(sm.getSpellingLineNumber(src_loc)));
         Store_field(pos, 2, Val_int(file_offset - column));
         Store_field(pos, 3, Val_int(file_offset));
 
-        return pos;
+        CAMLreturn(pos);
     }
 
     value readFundamentalType (QualType qtype, SourceLocation loc)
     {
-        string t = qtype.getAsString();
+        CAMLparam0 ();
+
+        string t(qtype.getAsString());
         fund_types[loc] = t;
-        value tname = caml_copy_string(t.c_str());
-        value pos = position_of_SourceLocation(loc);
-        value ret = caml_alloc(2, 0);
+
+        CAMLlocal3 (tname, pos, ret);
+        tname = caml_copy_string(t.c_str());
+        pos = position_of_SourceLocation(loc);
+        ret = caml_alloc(2, 0);
         Store_field(ret, 0, pos);
         Store_field(ret, 1, tname);
-        return ret;
+
+        CAMLreturn(ret);
     }
 
     value readType (QualType fulltype, SourceLocation loc)
     {
+        CAMLparam0 ();
+
         // ---
         // FIXME: Shouldn't get rid of qualifications.  Need to deal with them.
         QualType qtype = fulltype.getUnqualifiedType();
@@ -149,12 +163,13 @@ private:
 
         if (type->isPointerType()) {
             // CT_Pointer
-            value pointee = readType(type->getPointeeType(), loc);
-            value pos = position_of_SourceLocation(loc);
-            value ret = caml_alloc(2, 1);
+            CAMLlocal3 (pointee, pos, ret);
+            pointee = readType(type->getPointeeType(), loc);
+            pos = position_of_SourceLocation(loc);
+            ret = caml_alloc(2, 1);
             Store_field(ret, 0, pos);
             Store_field(ret, 1, pointee);
-            return ret;
+            CAMLreturn(ret);
         } else if (type->isFunctionNoProtoType()) {
             // FIXME: don't know what to do with this.
             outs() <<
@@ -164,9 +179,11 @@ private:
             // CT_Function
             const FunctionProtoType *ftype = type->castAs<FunctionProtoType>();
 
-            value param_list = Val_int(0);
+            CAMLlocal1 (param_list);
+            param_list = Val_int(0);
             for (const QualType param : ftype->param_types()) {
-                value tmp = caml_alloc(2, 0);
+                CAMLlocal1 (tmp);
+                tmp = caml_alloc(2, 0);
                 Store_field(tmp, 0, readType(param, loc));
                 Store_field(tmp, 1, param_list);
                 param_list = tmp;
@@ -180,7 +197,7 @@ private:
             Store_field(prototype, 1, param_list);
             Store_field(prototype, 2, Val_int(ftype->isVariadic()));
             Store_field(prototype, 3, rettype);
-            return prototype;
+            CAMLreturn(prototype);
         } else if (type->isArrayType()) {
             // CT_Array
 
@@ -190,52 +207,58 @@ private:
                 // Certain types are listed as arrays of themselves.  These
                 // are typically types that have some builtin meaning -
                 // language hacks.  Treat them as fundamental types.
-                return readFundamentalType(qtype, loc);
+                CAMLreturn(readFundamentalType(qtype, loc));
             }
 
-            value pos = position_of_SourceLocation(loc);
+            CAMLlocal1 (pos);
+            pos = position_of_SourceLocation(loc);
             if (type->isConstantArrayType()) {
                 const ConstantArrayType *atype =
                     static_cast<const ConstantArrayType*>(type);
-                value subtype = readType(atype->getElementType(), loc);
-                value ret = caml_alloc(3, 4);
+                CAMLlocal2 (subtype, ret);
+                subtype = readType(atype->getElementType(), loc);
+                ret = caml_alloc(3, 4);
                 Store_field(ret, 0, pos);
                 Store_field(ret, 1, subtype);
                 Store_field(ret, 2, Val_int(atype->getSize().getZExtValue()));
-                return ret;
+                CAMLreturn(ret);
             } else if (type->isIncompleteArrayType()) {
                 const IncompleteArrayType *atype =
                     static_cast<const IncompleteArrayType*>(type);
-                value subtype = readType(atype->getElementType(), loc);
-                value ret = caml_alloc(3, 4);
+                CAMLlocal2 (subtype, ret);
+                subtype = readType(atype->getElementType(), loc);
+                ret = caml_alloc(3, 4);
                 Store_field(ret, 0, pos);
                 Store_field(ret, 1, subtype);
                 Store_field(ret, 2, Val_int(0));
-                return ret;
+                CAMLreturn(ret);
             } else {
                 outs() << "Internal error: unhandled array type.\n";
                 exit(1);
             }
         } else {
             // CT_TypeName
-            return readFundamentalType(qtype, loc);
+            CAMLreturn(readFundamentalType(qtype, loc));
         }
     }
 
     void declareFunction (NamedDecl *decl)
     {
+        CAMLparam0 ();
+
         const FunctionDecl *fdecl = decl->getAsFunction();
 
-        value pos = position_of_SourceLocation(fdecl->getLocStart());
-        value fname =
-            caml_copy_string(fdecl->getQualifiedNameAsString().c_str());
+        CAMLlocal3 (pos, fname, param_list);
+        pos = position_of_SourceLocation(fdecl->getLocStart());
+        fname = caml_copy_string(fdecl->getQualifiedNameAsString().c_str());
 
         // Read the parameters into a list.
-        value param_list = Val_int(0);
+        param_list = Val_int(0);
         for (const ParmVarDecl *param : fdecl->parameters()) {
-            value param_n = readType(param->getOriginalType(),
-                                     param->getLocStart());
-            value tmp = caml_alloc(2, 0);
+            CAMLlocal2 (param_n, tmp);
+            param_n = readType(param->getOriginalType(),
+                               param->getLocStart());
+            tmp = caml_alloc(2, 0);
             Store_field(tmp, 0, param_n);
             Store_field(tmp, 1, param_list);
             param_list = tmp;
@@ -243,23 +266,30 @@ private:
         param_list = reverse_list(param_list);
 
         // Function's return type.
-        value rettp = readType(fdecl->getReturnType(),
-                               fdecl->getReturnTypeSourceRange().getBegin());
-        value is_variadic = Val_bool(fdecl->isVariadic());
+        CAMLlocal2 (rettp, is_variadic);
+        rettp = readType(fdecl->getReturnType(),
+                         fdecl->getReturnTypeSourceRange().getBegin());
+        is_variadic = Val_bool(fdecl->isVariadic());
 
         // CV_Function of string * ctype list * ctype
-        value fcn = caml_alloc(5, 0);
+        CAMLlocal1 (fcn);
+        fcn = caml_alloc(5, 0);
         Store_field(fcn, 0, pos);
         Store_field(fcn, 1, fname);
         Store_field(fcn, 2, param_list);
         Store_field(fcn, 3, is_variadic);
         Store_field(fcn, 4, rettp);
 
+        // ESCAPE
         cvalues.push_back(fcn);
+
+        CAMLreturn0;
     }
 
     void declareRecord (NamedDecl *decl)
     {
+        CAMLparam0 ();
+
         string name = decl->getNameAsString();
         SourceLocation loc = decl->getLocStart();
 
@@ -270,38 +300,38 @@ private:
                 // and I must be doing something wrong, here.
 
                 // For now, ignore records like this.
-                return;
+                CAMLreturn0;
             }
         }
-        value pos = position_of_SourceLocation(loc);
-        value sname = caml_copy_string(name.c_str());
-        value type_opt;
+        CAMLlocal3 (pos, sname, type_opt);
+        pos = position_of_SourceLocation(loc);
+        sname = caml_copy_string(name.c_str());
 
         RecordDecl *record = static_cast<RecordDecl*>(decl);
         RecordDecl *defn = record->getDefinition();
         if (NULL != defn) {
             // Read the record's fields.
-            value rec_kind = Val_int(defn->getTypeForDecl()->isUnionType()
-                                     ? 1 : 0);
-            value fields = Val_int(0);
+            CAMLlocal2 (rec_kind, fields);
+            rec_kind = Val_int(defn->getTypeForDecl()->isUnionType() ? 1 : 0);
+            fields = Val_int(0);
             for (const FieldDecl *field : defn->fields()) {
-                value fpos = position_of_SourceLocation(field->getLocStart());
-                value fname =
-                    caml_copy_string(field->getNameAsString().c_str());
-                value ftype = readType(field->getType(),
-                                       field->getLocStart());
+                CAMLlocal5 (fpos, fname, ftype, frec, tmp);
+                fpos = position_of_SourceLocation(field->getLocStart());
+                fname = caml_copy_string(field->getNameAsString().c_str());
+                ftype = readType(field->getType(), field->getLocStart());
 
-                value frec = caml_alloc(3, 2);
+                frec = caml_alloc(3, 2);
                 Store_field(frec, 0, fpos);
                 Store_field(frec, 1, fname);
                 Store_field(frec, 2, ftype);
 
-                value tmp = fields;
+                tmp = fields;
                 fields = caml_alloc(2, 0);
                 Store_field(fields, 0, frec);
                 Store_field(fields, 1, tmp);
             }
-            value struct_def = caml_alloc(2, 2);
+            CAMLlocal1 (struct_def);
+            struct_def = caml_alloc(2, 2);
             Store_field(struct_def, 0, rec_kind);
             Store_field(struct_def, 1, reverse_list(fields));
             type_opt = caml_alloc(1, 0); // OCaml: Some t
@@ -310,47 +340,62 @@ private:
             // No definition for this record.
             type_opt = Val_int(0); // OCaml: None
         }
-        value sdecl = caml_alloc(3, 1);
+        CAMLlocal1 (sdecl);
+        sdecl = caml_alloc(3, 1);
         Store_field(sdecl, 0, pos);
         Store_field(sdecl, 1, sname);
         Store_field(sdecl, 2, type_opt);
+        // ESCAPE
         cvalues.push_back(sdecl);
+
+        CAMLreturn0;
     }
 
     void declareTypedef (NamedDecl *decl)
     {
+        CAMLparam0 ();
         SourceLocation src_loc = decl->getLocStart();
-        value pos = position_of_SourceLocation(src_loc);
-        value tname = caml_copy_string(decl->getNameAsString().c_str());
+        CAMLlocal3 (pos, tname, t);
+        pos = position_of_SourceLocation(src_loc);
+        tname = caml_copy_string(decl->getNameAsString().c_str());
 
         TypedefNameDecl *typedefdecl = static_cast<TypedefNameDecl*>(decl);
-        value t = readType(typedefdecl->getUnderlyingType(), src_loc);
+        t = readType(typedefdecl->getUnderlyingType(), src_loc);
 
         // OCaml: Some type
-        value SomeT = caml_alloc(1, 0);
+        CAMLlocal2 (SomeT, tdecl);
+        SomeT = caml_alloc(1, 0);
         Store_field(SomeT, 0, t);
 
-        value tdecl = caml_alloc(3, 1);
+        tdecl = caml_alloc(3, 1);
         Store_field(tdecl, 0, pos);
         Store_field(tdecl, 1, tname);
         Store_field(tdecl, 2, SomeT);
+        // ESCAPE
         cvalues.push_back(tdecl);
+
+        CAMLreturn0;
     }
 
     void declareVariable (NamedDecl *decl)
     {
+        CAMLparam0 ();
         SourceLocation src_loc = decl->getLocStart();
-        value pos = position_of_SourceLocation(src_loc);
-        value vname = caml_copy_string(decl->getNameAsString().c_str());
+        CAMLlocal4 (pos, vname, vtype, vdecl);
+        pos = position_of_SourceLocation(src_loc);
+        vname = caml_copy_string(decl->getNameAsString().c_str());
         ValueDecl *valuedecl = static_cast<ValueDecl*>(decl);
-        value vtype = readType(valuedecl->getType(), src_loc);
+        vtype = readType(valuedecl->getType(), src_loc);
 
-        value vdecl = caml_alloc(4, 2);
+        vdecl = caml_alloc(4, 2);
         Store_field(vdecl, 0, pos);
         Store_field(vdecl, 1, vname);
         Store_field(vdecl, 2, vtype);
         Store_field(vdecl, 3, Val_bool(decl->hasExternalFormalLinkage()));
+        // ESCAPE
         cvalues.push_back(vdecl);
+
+        CAMLreturn0;
     }
 };
 
@@ -382,6 +427,7 @@ public:
 
 value cimport_file (const char *filename, int argc, const char **argv)
 {
+    CAMLparam0();
     std::vector<std::string> source_files;
     source_files.push_back(filename);
 
@@ -395,10 +441,14 @@ value cimport_file (const char *filename, int argc, const char **argv)
     }
 
     // Build the list of cvalues.
-    value cv_list = Val_int(0);
-    for (value cv : cvalues) {
-        value tmp = caml_alloc(2, 0);
-        Store_field(tmp, 0, cv);
+    CAMLlocal1 (cv_list);
+    cv_list = Val_int(0);
+    for (vector<value>::iterator cvi = cvalues.begin();
+         cvi != cvalues.end();
+         ++cvi) {
+        CAMLlocal1 (tmp);
+        tmp = caml_alloc(2, 0);
+        Store_field(tmp, 0, *cvi);
         Store_field(tmp, 1, cv_list);
         cv_list = tmp;
     }
@@ -406,10 +456,12 @@ value cimport_file (const char *filename, int argc, const char **argv)
 
     // Empty the cvalues and fund_types sets in case this function gets called
     // again.  It would be "very bad" to have stale data lying around.
+    // ESCAPE?
     cvalues.clear();
+    // ESCAPE?
     fund_types.clear();
 
-    return cv_list;
+    CAMLreturn(cv_list);
 }
 
 /** import_c_file f p: Import the C header file, f, using the include
@@ -418,10 +470,12 @@ value cimport_file (const char *filename, int argc, const char **argv)
 extern "C"
 CAMLprim value cimport_import_c_file (value filename, value paths)
 {
+    CAMLparam2 (filename, paths);
     char *file = String_val(filename);
 
     std::vector<std::string> path_vec;
     for ( ; paths != Val_int(0); paths = Field(paths, 1)) {
+        // ESCAPE?
         path_vec.push_back(std::string("-I") + String_val(Field(paths, 0)));
     }
     path_vec.push_back(std::string("-D__def=1"));
@@ -434,5 +488,5 @@ CAMLprim value cimport_import_c_file (value filename, value paths)
         argv[i] = path_vec[i - 3].c_str();
     }
 
-    return cimport_file(file, argc, argv);
+    CAMLreturn(cimport_file(file, argc, argv));
 }
