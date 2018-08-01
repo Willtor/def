faf6c101d418c0e2d2d87eef191b74ce1ae922e0
William M. Leiserson <willtor@mit.edu>
2018-08-01 17:50:20 -0400

    Bump version id.

--- version_info.sh
+++ version_info.sh
@@ -4,8 +4,8 @@
 # across the compiler and libraries.
 
 VERSION_MAJOR=0
-VERSION_MINOR=14
-VERSION_PATCH=21
+VERSION_MINOR=15
+VERSION_PATCH=0
 VERSION_SUFFIX=a
 VERSION_BUILD=`git rev-parse HEAD | cut -c -12`
 BUILD_DATE=`date -I`
