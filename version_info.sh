9ca336b01ef844ff75cb90fdbf2876a3ae50242d
William M. Leiserson <willtor@mit.edu>
2019-05-28 11:57:44 -0400

    Bump minor version.  The last bump was a mistake... whatever.

--- version_info.sh
+++ version_info.sh
@@ -4,8 +4,8 @@
 # across the compiler and libraries.
 
 VERSION_MAJOR=0
-VERSION_MINOR=18
-VERSION_PATCH=8
+VERSION_MINOR=20
+VERSION_PATCH=0
 VERSION_SUFFIX=a
 VERSION_BUILD=`git rev-parse HEAD | cut -c -12`
 BUILD_DATE=`date -I`
